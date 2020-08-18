f33448132052d4c0f4c16e05da826076d3d2dd47
hatz7505 <harryteplow@hotmail.com>
2020-08-17 17:14:03 -0700

    cleaned up code and added jquery

--- game.js
+++ game.js
@@ -1,119 +1,152 @@
-
-var kweenPics = new Array("ad1.png", "ad2.png", "al1.png", "al2.png", "ar1.png", "ar2.png", "be1.png", "be2.png", "bi1.png", "bi2.png", "de1.png", "de2.png", "du1.png", "du2.png", "ga1.png", "ga2.png", "la1.png", "la2.png", "li1.png", "li2.png", "ri1.png", "ri2.png", "ro1.png", "ro2.png");
-
+var kweenPics = new Array(
+  "./kween-images/ad1.png",
+  "./kween-images/ad2.png",
+  "./kween-images/al1.png",
+  "./kween-images/al2.png",
+  "./kween-images/ar1.png",
+  "./kween-images/ar2.png",
+  "./kween-images/be1.png",
+  "./kween-images/be2.png",
+  "./kween-images/bi1.png",
+  "./kween-images/bi2.png",
+  "./kween-images/de1.png",
+  "./kween-images/de2.png",
+  "./kween-images/du1.png",
+  "./kween-images/du2.png",
+  "./kween-images/ga1.png",
+  "./kween-images/ga2.png",
+  "./kween-images/la1.png",
+  "./kween-images/la2.png",
+  "./kween-images/li1.png",
+  "./kween-images/li2.png",
+  "./kween-images/ri1.png",
+  "./kween-images/ri2.png",
+  "./kween-images/ro1.png",
+  "./kween-images/ro2.png"
+);
 
 function startGame() {
-    for (let i = 0; i < 24; i++) {
-        var container = document.getElementById('cardContainer');
-        let newCard = document.createElement('div');
-        newCard.classList.add('card');
-        let backFace = document.createElement('div');
-        backFace.classList.add('back');
-        backFace.innerHTML = '<img src="purple-card.png" class="purpleCard"/>';
-        let frontFace = document.createElement('div');
-        frontFace.classList.add('front');
-        
-        choosePic(frontFace, kweenPics);
+  for (let i = 0; i < 24; i++) {
+    var container = $("#cardContainer");
+    let newCard = document.createElement("div");
+    newCard.classList.add("card");
+    let backFace = document.createElement("div");
+    backFace.classList.add("back");
+    backFace.innerHTML = '<img src="purple-card.png" class="purpleCard"/>';
+    let frontFace = document.createElement("div");
+    frontFace.classList.add("front");
 
-        newCard.appendChild(backFace);
-        newCard.appendChild(frontFace);
-        container.appendChild(newCard);
+    choosePic(frontFace, kweenPics);
 
-        newCard.addEventListener('click', function () {
-            flipCard(newCard);
-        })
-    }
-    showBestScore();
+    newCard.appendChild(backFace);
+    newCard.appendChild(frontFace);
+    container.append(newCard);
+
+    newCard.addEventListener("click", function () {
+      flipCard(newCard);
+    });
+  }
+  showBestScore();
 }
 function choosePic(div, arr) {
-    let idx = Math.floor(Math.random() * arr.length)
-    var pic = document.createElement('img');
-    pic.src = arr[idx];
-    pic.classList.add('kweenPic');
-    arr.splice(idx, 1);
-    div.appendChild(pic);
+  let idx = Math.floor(Math.random() * arr.length);
+  var pic = document.createElement("img");
+  pic.src = arr[idx];
+  pic.classList.add("kweenPic");
+  arr.splice(idx, 1);
+  div.appendChild(pic);
 }
 
 let flipped = false;
-let card1, card2, firstCard1, firstCard2, secondCard1, secondCard2
+let card1,
+  card2,
+  firstCardFirstLetter,
+  firstCardSecondLetter,
+  secondCardFirstLetter,
+  secondCardSecondLetter;
 
 function flipCard(card) {
-    var pic = card.childNodes[1].childNodes[0].src
-    
-    if (!card.classList.contains('matched') && !card.classList.contains('first')) {
-        addCount();
+  var imagePath = card.childNodes[1].childNodes[0].src;
 
-        card.classList.add('clicked');
+  if (
+    !card.classList.contains("matched") &&
+    !card.classList.contains("first")
+  ) {
+    addCount();
 
-        if (!flipped) {
-            flipped = true;
-            firstCard1 = pic[39];
-            firstCard2 = pic[40];
-            card1 = card;
-            card1.classList.add('first');
+    card.classList.add("clicked");
 
-        } else {
-            flipped = false;
-            secondCard1 = pic[39];
-            secondCard2 = pic[40];
-            card2 = card;
-            card1.classList.remove('first');
-
-            if (firstCard1 == secondCard1 && firstCard2 == secondCard2) {
-                card1.classList.add('matched');
-                card2.classList.add('matched');
-            } else {
-                setTimeout(function() {
-                card1.classList.remove('clicked');
-                card2.classList.remove('clicked');
-                }, 1000);
-            }
-        }
-        
+    if (!flipped) {
+      flipped = true;
+      firstCardFirstLetter = imagePath[79];
+      firstCardSecondLetter = imagePath[80];
+      card1 = card;
+      card1.classList.add("first");
+    } else {
+      flipped = false;
+      secondCardFirstLetter = imagePath[79];
+      secondCardSecondLetter = imagePath[80];
+      card2 = card;
+      card1.classList.remove("first");
+      if (
+        firstCardFirstLetter == secondCardFirstLetter &&
+        firstCardSecondLetter == secondCardSecondLetter
+      ) {
+        card1.classList.add("matched");
+        card2.classList.add("matched");
+      } else {
+        setTimeout(function () {
+          card1.classList.remove("clicked");
+          card2.classList.remove("clicked");
+        }, 1000);
+      }
     }
-    finish();
+  }
+  isFinished();
 }
-var count = 1
+var count = 1;
 function addCount() {
-    let newCount = count++;
-    document.getElementById('count').innerHTML = newCount;
-  
+  let newCount = count++;
+  document.getElementById("count").innerHTML = newCount;
 }
 
-function finish() {
-    var cards = document.getElementById('cardContainer').childNodes;
-    for (let i = 1; i < 25; i++) {
-        if (!(cards[i].classList.contains('matched'))) {
-            return;
-        }
+function isFinished() {
+  var cards = document.getElementById("cardContainer").childNodes;
+  for (let i = 0; i < 24; i++) {
+    if (!cards[i].classList.contains("matched")) {
+      return;
     }
-    finished();
+  }
+  finished();
 }
 
 function finished() {
-    var body = document.getElementsByClassName('bg')[0];
-    let finished = document.createElement('div');
-    finished.classList.add('finished');
-    finished.innerHTML = 'Yaaas Kween!';
-    body.appendChild(finished);
+  var body = document.getElementsByClassName("bg")[0];
+  let finished = document.createElement("div");
+  finished.classList.add("finished");
+  finished.innerHTML = "Yaaas Kween!";
+  body.appendChild(finished);
 
-    isBestScore();
+  isBestScore();
 }
 
 function isBestScore() {
-    if (count < localStorage.getItem('bestScore') || localStorage.getItem('bestScore') == null) {
-        let score = document.getElementById('bestScore');
-        score.innerHTML = 'Best Score = ' + (count-1);
-        localStorage.setItem('bestScore', count);
-    }
+  if (
+    count < localStorage.getItem("bestScore") ||
+    localStorage.getItem("bestScore") == null
+  ) {
+    let score = document.getElementById("bestScore");
+    score.innerHTML = "Best Score = " + (count - 1);
+    localStorage.setItem("bestScore", count);
+  }
 }
 
 function showBestScore() {
-    if (localStorage.getItem('bestScore') == null) {
-        let score = document.getElementById('bestScore');
-        score.innerHTML = 'Best Score = --';
-    } else {
-        let score = document.getElementById('bestScore');
-        score.innerHTML = 'Best Score = ' + (localStorage.getItem('bestScore')-1); 
-    }
+  if (localStorage.getItem("bestScore") == null) {
+    let score = document.getElementById("bestScore");
+    score.innerHTML = "Best Score = --";
+  } else {
+    let score = document.getElementById("bestScore");
+    score.innerHTML = "Best Score = " + (localStorage.getItem("bestScore") - 1);
+  }
 }
